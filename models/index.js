a4363947451f0a5d7fcf2ce0d1525debb3a8c3d5
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-11-03 12:09:52 -0700

    changes to server.js

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const Traveller = require("./Traveller");
 const Trips = require("./Trips");
 const Location = require("./Location");
 
-Traveller.belongsToMany(Location, { through: Trips });
-Location.belongsToMany(Traveller, { through: Trips });
+Traveller.belongsToMany(Location, { through: Trips, unique: false });
+Location.belongsToMany(Traveller, { through: Trips, unique: false });
 
 module.exports = { Traveller, Location, Trips };
